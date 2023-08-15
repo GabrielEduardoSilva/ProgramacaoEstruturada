f527676fec3a97066fe1a0e13022533cc77a9d5f
GabrielEduardoSilva <gaebds@gmail.com>
2023-08-15 12:30:53 -0300

    Create 6.c

---
+++ Atividade2-OperadoresAritmeticos/Atividade2-OperadoresAritmeticos/6.c
@@ -0,0 +1,24 @@
+//
+//  6.c
+//  Atividade2-OperadoresAritmeticos
+//
+//  Created by Gabriel Eduardo on 15/08/23.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    float raio;
+
+    printf("Digite o raio do circulo: ");
+    scanf("%f", &raio);
+
+    // Cálculo da área do círculo
+    float area = M_PI * pow(raio, 2);
+
+    // Apresentação do resultado
+    printf("A area do circulo com raio %.2f eh %.2f\n", raio, area);
+
+    return 0;
+}
