98728e69513ff87e7536c758d56a85d67e1dd249
GabrielEduardoSilva <gaebds@gmail.com>
2023-08-28 13:32:09 -0300

    Código 4 Finalizado

---
+++ Atividade4-OperadoresRelacionais/Atividade4-OperadoresRelacionais/4.c
@@ -0,0 +1,40 @@
+//
+//  4.c
+//  Atividade4-OperadoresRelacionais
+//
+//  Created by Gabriel Eduardo on 28/08/23.
+//
+
+// Dados três valores A, B e C, construa um algoritmo, que imprima os valores de forma descendente (do maior para o menor).
+
+#include <stdio.h>
+
+int main(void) {
+    float A, B, C;
+
+    printf("Digite três valores (A B C): ");
+    scanf("%f %f %f", &A, &B, &C);
+
+    if (A >= B && A >= C) {
+        if (B >= C) {
+            printf("Valores em ordem descendente: %.2f %.2f %.2f\n", A, B, C);
+        } else {
+            printf("Valores em ordem descendente: %.2f %.2f %.2f\n", A, C, B);
+        }
+    } else if (B >= A && B >= C) {
+        if (A >= C) {
+            printf("Valores em ordem descendente: %.2f %.2f %.2f\n", B, A, C);
+        } else {
+            printf("Valores em ordem descendente: %.2f %.2f %.2f\n", B, C, A);
+        }
+    } else {
+        if (A >= B) {
+            printf("Valores em ordem descendente: %.2f %.2f %.2f\n", C, A, B);
+        } else {
+            printf("Valores em ordem descendente: %.2f %.2f %.2f\n", C, B, A);
+        }
+    }
+
+    return 0;
+}
+
