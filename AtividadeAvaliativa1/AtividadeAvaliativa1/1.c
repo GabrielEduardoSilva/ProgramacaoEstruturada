c37d394b56787b23dcac3509f3b4fbd8ca34bed2
GabrielEduardoSilva <gaebds@gmail.com>
2023-09-25 20:25:36 -0300

    Código 1 Finalizado

---
+++ AtividadeAvaliativa1/AtividadeAvaliativa1/1.c
@@ -0,0 +1,26 @@
+//
+//  1.c
+//  AtividadeAvaliativa1
+//
+//  Created by Gabriel Eduardo on 25/09/23.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x, resultado;
+
+    printf("Digite o valor de x: ");
+    scanf("%lf", &x);
+
+    if (x * x - 16 >= 0) {
+        resultado = (5 * x + 3) / sqrt(x * x - 16);
+        printf("f(x) = %lf\n", resultado);
+    } else {
+        printf("A raiz quadrada do denominador não é real. Impossível calcular f(x).\n");
+    }
+
+    return 0;
+}
+
