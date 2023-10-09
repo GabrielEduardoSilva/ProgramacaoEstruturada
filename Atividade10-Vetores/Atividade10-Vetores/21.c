f1cd93ed852fbd4ef37d9f566b90b117af60a9d8
GabrielEduardoSilva <gaebds@gmail.com>
2023-10-09 20:22:52 -0300

    Código 21 Finalizado

---
+++ Atividade10-Vetores/Atividade10-Vetores/21.c
@@ -0,0 +1,35 @@
+//
+//  21.c
+//  Atividade10-Vetores
+//
+//  Created by Gabriel Eduardo on 09/10/23.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int A[10], B[10], C[10];
+
+    printf("Digite os 10 números inteiros do vetor A:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    printf("Digite os 10 números inteiros do vetor B:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &B[i]);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        C[i] = A[i] - B[i];
+    }
+
+    printf("Vetor C (C = A - B):\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", C[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
