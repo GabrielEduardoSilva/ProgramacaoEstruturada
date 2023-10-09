546cf0090e657415d210778af6cd8ab184b4a726
GabrielEduardoSilva <gaebds@gmail.com>
2023-10-09 13:22:39 -0300

    Código 13 Finalizado

--- Atividade9-Vetores/Atividade9-Vetores/13.c
+++ Atividade9-Vetores/Atividade9-Vetores/13.c
@@ -1,5 +1,5 @@
 //
-//  12.c
+//  13.c
 //  Atividade9-Vetores
 //
 //  Created by Gabriel Eduardo on 09/10/23.
@@ -9,8 +9,7 @@
 
 int main(void) {
     int valores[5];
-    int maior = 0, menor = 0, soma = 0;
-    float media;
+    int maior = 0, menor = 0, posicaoMaior = 0, posicaoMenor = 0;
 
     printf("Digite 5 valores inteiros:\n");
     for (int i = 0; i < 5; i++) {
@@ -18,28 +17,21 @@ int main(void) {
 
         if (i == 0) {
             maior = menor = valores[i];
+            posicaoMaior = posicaoMenor = i;
         } else {
             if (valores[i] > maior) {
                 maior = valores[i];
+                posicaoMaior = i;
             }
             if (valores[i] < menor) {
                 menor = valores[i];
+                posicaoMenor = i;
             }
         }
-
-        soma += valores[i];
     }
 
-    media = (float)soma / 5;
-
-    printf("Valores lidos:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("%d ", valores[i]);
-    }
-    printf("\n");
-    printf("Maior valor: %d\n", maior);
-    printf("Menor valor: %d\n", menor);
-    printf("Média dos valores: %.2f\n", media);
+    printf("Posição do maior valor: %d\n", posicaoMaior);
+    printf("Posição do menor valor: %d\n", posicaoMenor);
 
     return 0;
 }
