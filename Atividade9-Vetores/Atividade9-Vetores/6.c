16abdd285f5bccb52170fedd6835ce715bfd004e
GabrielEduardoSilva <gaebds@gmail.com>
2023-10-09 13:05:22 -0300

    Código 6 Finalizado

---
+++ Atividade9-Vetores/Atividade9-Vetores/6.c
@@ -0,0 +1,36 @@
+//
+//  6.c
+//  Atividade9-Vetores
+//
+//  Created by Gabriel Eduardo on 09/10/23.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int vetor[10];
+    int maior, menor;
+
+    printf("Digite 10 valores inteiros:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    maior = menor = vetor[0];
+
+    for (int i = 1; i < 10; i++) {
+        if (vetor[i] > maior) {
+            maior = vetor[i];
+        }
+        if (vetor[i] < menor) {
+            menor = vetor[i];
+        }
+    }
+
+    // Exibição do maior e do menor elemento
+    printf("O maior elemento do vetor é: %d\n", maior);
+    printf("O menor elemento do vetor é: %d\n", menor);
+
+    return 0;
+}
+
